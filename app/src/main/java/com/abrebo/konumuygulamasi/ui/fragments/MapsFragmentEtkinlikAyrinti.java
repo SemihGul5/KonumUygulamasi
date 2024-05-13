21a6a7005006d66c62aa69e43d0f017db570d64a
semihgul5 <semih.gul099@gmail.com>
2024-05-13 16:49:33 +0300

    updates

--- app/src/main/java/com/abrebo/konumuygulamasi/ui/fragments/MapsFragmentEtkinlikAyrinti.java
+++ app/src/main/java/com/abrebo/konumuygulamasi/ui/fragments/MapsFragmentEtkinlikAyrinti.java
@@ -1,8 +1,11 @@
 package com.abrebo.konumuygulamasi.ui.fragments;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
@@ -11,6 +14,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -32,6 +36,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsFragmentEtkinlikAyrinti extends Fragment {
     private FragmentMapsEtkinlikAyrintiBinding binding;
     private String enlem,boylam;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private ActivityResultLauncher<String> permissionLauncher;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
         @Override
@@ -42,6 +49,13 @@ public class MapsFragmentEtkinlikAyrinti extends Fragment {
                     .title("Etkinlik Konumu");
             googleMap.addMarker(markerOptions);
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+            // Konum izni kontrolü
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+            } else {
+                // Konum al
+                getLocation();
+            }
         }
     };
 
@@ -78,6 +92,7 @@ public class MapsFragmentEtkinlikAyrinti extends Fragment {
             Criteria criteria = new Criteria();
             String provider = locationManager.getBestProvider(criteria, false);
             if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
                 return;
             }
             Location location = locationManager.getLastKnownLocation(provider);
@@ -97,7 +112,18 @@ public class MapsFragmentEtkinlikAyrinti extends Fragment {
             Log.i("Mesaj",e.getMessage());
         }
     }
-
+    private void registerLauncher() {
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                // Konum izni verilmiş, işlem yap
+                if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                }
+            } else {
+                Toast.makeText(requireContext(), "Konum izni verilmedi", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -105,6 +131,23 @@ public class MapsFragmentEtkinlikAyrinti extends Fragment {
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapEtkinlikPin);
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
+        }
+        registerLauncher();
+    }
+    private void getLocation() {
+        locationManager = (LocationManager) requireContext().getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager != null) {
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    // Konum değiştiğinde çalışacak kodlar
+                }
+            };
+            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
         }
     }
 }
